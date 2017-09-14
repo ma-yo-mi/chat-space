266fcffbf210e4a116a507d13e2392bc2f723f60
ma-yo-mi <masayoshizz@yahoo.co.jp>
2017-09-14 17:35:55 +0900

    set devise and others(flash) v3

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,23 +6,4 @@ before_action :move_to_index, except: :index
 
   def new
   end
-
-  def create
-    @member = Member.new(member_params)
-    if @member.save
-      flash[:notice] = "メッセージ送信成功"
-      redirect_to members_path
-    else
-      flash.now[:alert] = 'メッセージの送信失敗'
-      render action: :index
-    end
-  end
-
-
-
-private
-
-  def member_params
-    params.require(:message).permit(:image)
-  end
 end
