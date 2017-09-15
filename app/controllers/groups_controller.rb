4d2403f8f8974d857059d2f1d0242c80b048b29a
ma-yo-mi <masayoshizz@yahoo.co.jp>
2017-09-15 09:17:14 +0900

    create controller & model v3

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,9 +6,10 @@ class GroupsController < ApplicationController
     @group = Group.new(group_params)
     if @group.save
       redirect_to_group_path
-      else
-        render action: :new
-
+    else
+      render action: :new
+    end
+# TODO: 作動するかview作成したら確認要(controller & model)
   end
 
   def edit
@@ -17,7 +18,6 @@ class GroupsController < ApplicationController
   private
   def group_params
     params.permit(:name)
+# TODO: 作動するかview作成したら確認要(controller & model)
   end
 end
-
-
